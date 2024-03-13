3766a727dda6a38644775d74f2ba79430192fa48
IsraelGalvez <galvez.rli.51@gmail.com>
2024-03-13 11:42:20 -0700

    No esta listo contactos, se arreglaron algunos alt de las imagenes

--- rogafin/apps/core/static/core/javascript/index.js
+++ rogafin/apps/core/static/core/javascript/index.js
@@ -1,6 +1,9 @@
-nav = document.querySelector(".nav")
-header = document.querySelector(".header")
-menuButton = document.querySelector(".menuButton")
+const nav = document.querySelector(".nav")
+const header = document.querySelector(".header")
+const menuButton = document.querySelector(".menuButton")
+const buttonLefts = document.querySelectorAll('.main_section4_container_items_images_slider_buttonLeft');
+const buttonRights = document.querySelectorAll('.main_section4_container_items_images_slider_buttonRight');
+const sliders = document.querySelectorAll('.main_section4_container_items_images_slider_div');
 
 menuButton.addEventListener("click", () => {
     if (nav.style.display === "none" || window.getComputedStyle(nav).getPropertyValue("display") === "none") {
@@ -14,5 +17,29 @@ menuButton.addEventListener("click", () => {
         }, 600); // Espera a que termine la transición antes de ocultar la navegación completamente
         header.style.height = "84.3px";
     }
+});
+
+sliders.forEach((slider, index) => {
+    const slide = slider.querySelector('.main_section4_container_items_images_figure');
+    const slideStyle = window.getComputedStyle(slide);
+    const slideWidth = slide.offsetWidth 
+        + parseInt(slideStyle.getPropertyValue('padding-left')) 
+        + parseInt(slideStyle.getPropertyValue('padding-right'))
+        + parseInt(slideStyle.getPropertyValue('margin-left')) 
+        + parseInt(slideStyle.getPropertyValue('margin-right'));
+
+    buttonRights[index].addEventListener('click', function() {
+        slider.scrollBy({
+            left: slideWidth,
+            behavior: 'smooth'
+        });
+    });
+
+    buttonLefts[index].addEventListener('click', function() {
+        slider.scrollBy({
+            left: -slideWidth,
+            behavior: 'smooth'
+        });
+    });
 });
 
